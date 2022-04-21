ea2047e3d58e6ffd01ef7ac918c45b8dabce31d9
hyexjun <hyebyjun@gmail.com>
2022-04-21 16:51:52 +0900

    초간단 TS 알아보기 테스트

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,2 +1,10 @@
 let yourName: string = 'kildong';
-yourName = 123;
+
+let nameList: string[] = ['kim', 'park'];
+
+let userID: string | number = 'kim123';
+
+type Customizing_typeName = string[] | number;
+
+type Member = [string, number, boolean];
+let mina: Member = ['미나', 26, false];
